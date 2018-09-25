f24d0a69af18ac39ff8ec7561cab07d761c3b24a
Michael Patel <mikepatel@users.noreply.github.com>
2018-09-24 23:39:47 -0400

    Updating notes

--- basic_eager_execution.py
+++ basic_eager_execution.py
@@ -1,13 +1,32 @@
 # 9/23/18
 
-#
-# using tf.keras
+# Tutorial that covers:
 # using eager execution
+# using GPU acceleration
+# using tensors
+# https://www.tensorflow.org/tutorials/eager/eager_basics
 
 # dataset:
 #
 
 # Notes:
+# tensor = multi-dimensional array that have data type 'dtype' and shape 'shape'
+# tensor vs numpy array (ndarray)
+#   - tensors can be backed by accelerator memory (GPU, TPU)
+#   - tensors are immutable
+# explicit device placement on CPU or GPU
+# tf.data.Dataset API -> build pipelines to feed data into model
+#   - with eager execution enabled, no need to create tf.data.Iterator object - just use Python iteration
 
 ################################################################################
 # IMPORTs
+import tensorflow as tf
+
+
+tf.enable_eager_execution()
+
+################################################################################
+print(tf.__version__)
+
+print("GPU available: " + str(tf.test.is_gpu_available()))
+
